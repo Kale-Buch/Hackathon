025cfceaa97a1927ef7ed3a73e44dfcc6899424a
FlynnHouse <flynn.house16@gmail.com>
2024-09-14 14:22:48 -0500

    Update HelloApplication.java

--- src/main/java/com/group/hackathon/hackathon/HelloApplication.java
+++ src/main/java/com/group/hackathon/hackathon/HelloApplication.java
@@ -27,6 +27,47 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+
+    String response = "I'm not sure how to help with that, partner.";
+    String lowerCommand = command.toLowerCase();
+
+    if (lowerCommand.startsWith("open ")) {
+        String app = command.substring(5);
+        response = "Alright, partner. Openin' up that " + app + " for ya faster than a quick-draw.";
+        openApp(app);
+    } else if (lowerCommand.contains("camera")) {
+        response = "Time to capture some memories, eh? Let's get that camera rollin' like tumbleweed.";
+        openCamera();
+    } else if (lowerCommand.contains("notes")) {
+        response = "Sure thing, buckaroo. I'll rustle up them notes for ya quicker than a prairie fire with a tailwind.";
+        openNotes();
+    } else if (lowerCommand.contains("send a text")) {
+        response = "Who're we sendin' a telegraph to, partner? Just say the word, and I'll get that message ridin' the wires.";
+        startSendingText();
+    } else if (lowerCommand.contains("directions")) {
+        response = "Where're we headed, trail boss? Let's get you on a stagecoach to where you wanna go, I'll plot us a course.";
+        getDirections();
+    } else if (lowerCommand.contains("restaurant")) {
+        response = "Fella, let's get you to the saloon before sundown. I hear they're servin' up grub that'll put meat on yer bones.";
+        findNearbyRestaurants();
+    } else if (lowerCommand.contains("alarm")) {
+        response = "You got it, partner. I'll have them chickens hollerin' at the right time. You'll be up with the sun, guaranteed.";
+        setAlarm(command);
+    } else if (lowerCommand.contains("schedule")) {
+        response = "Let me check yer trail map for the day, partner. Looks like you've got [list events] on the horizon " + getSchedule();
+    } else if (lowerCommand.contains("campfire")) {
+        response = "Comin' right up, cowpoke. I'll get that campfire cracklin' so you can rest your weary bones.";
+        playCampfireAmbiance();
+    } else if (lowerCommand.contains("weather")) {
+        response = "Let me take a gander at the sky for ya, partner. " + getWeatherForecast();
+    } else if (lowerCommand.contains("schedule")) {
+        response = "Let me check yer trail map for the day, partner. Looks like you've got [list events] on the horizon " + getSchedule();
+    } else if (lowerCommand.contains("cowboy name")) {
+        response = "Well, let me rustle up a proper trail name for ya... How about " + generateCowboyName() + "?";
+    }
+
+
+
     public static void main(String[] args) {
         launch();
     }

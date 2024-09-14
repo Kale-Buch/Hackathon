9efc3bbf5bedb987c5fbd81d62ba6aa005fadf5c
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-09-14 10:30:15 -0500

    Initial Commit

---
+++ hackathon/src/Main.java
@@ -0,0 +1,17 @@
+package hackathon.src;
+
+/**
+ * <h1>Hack-a-thon</h1><br>
+ * Group Members:
+ * <ul>
+ *     <li>Jack Terrell</li>
+ *     <li>Flynn House</li>
+ *     <li>Kale Buchanan</li>
+ *     <li>Sagar Basavaraju</li>
+ * </ul>
+ */
+public class Main {
+    public static void main(String[] args){
+        // Code here
+    }
+}

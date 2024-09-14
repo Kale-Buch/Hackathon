0948096e7ad3730b8e217920542860292d461568
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-09-14 13:38:51 -0500

    Updated Javadocs

--- src/main/java/com/group/hackathon/hackathon/HelloApplication.java
+++ src/main/java/com/group/hackathon/hackathon/HelloApplication.java
@@ -7,6 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Hack-A-Thon 9/14/2024-9/15/2024
+ * <br>
+ * <ul>
+ *     <li>Jack Terrell</li>
+ *     <li>FLynn House</li>
+ *     <li>Kale Buchanan</li>
+ *     <li>Sagar Basavaraju</li>
+ * </ul>
+ */
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
